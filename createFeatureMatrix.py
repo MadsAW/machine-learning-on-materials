ea3ec0bd4b6fcdfb1fff79d76b37db2d04351840
Simon Kamuk <simon.kamuk@gmail.com>
2018-09-25 06:44:13 +0200

    Ran calculation one more time with energies and atomicsymbolslist this time

--- createFeatureMatrix.py
+++ createFeatureMatrix.py
@@ -10,6 +10,9 @@ from ase.db import connect
 from prdf import prdf
 import numpy as np
 import time
+import datetime
+import pickle
+import os
 
 
 data = connect('oqmd12.db')
@@ -19,6 +22,10 @@ atomsList=[]
 for row in data.select():
     atomsList.append(data.get_atoms(row['id']))
     
+    
+now = datetime.datetime.now()
+folderName = now.strftime("%d-%m-%Y %H.%M")
+os.mkdir("Saved matrices/"+folderName)
 #%%
 """
 
@@ -75,6 +82,7 @@ start = time.time()
 featureMatrix=np.zeros(dim)
 
 
+
 index=0
 for atom in atomsList:
     X=prdf(atom,rs,dr,rmax)
@@ -83,11 +91,15 @@ for atom in atomsList:
     print(f"Progress: {index / dim[0] * 100:8.5f}%. Elapsed: {(time.time()-start)/60:5.2f} minutes. Remaining: {((time.time()-start)/ ((index +1) / dim[0]))/60 - (time.time()-start)/60:5.2f} minutes")
     index+=1
 
-np.save(f"featureMatrix.npy", featureMatrix)
+np.save("Saved matrices/"+folderName+"/featureMatrix.npy", featureMatrix)
+
 energies = [atom.get_potential_energy() for atom in atomsList]
 
-#HUSK AT TILFØJE SÅ atomicSymbolsList GEMMES I EN ANDEN FIL
-#HUSK AT TILFØJE SÅ energies GEMMES I EN ANDEN FIL
+with open("Saved matrices/"+folderName+"/pickledEnergies.txt", "wb") as pickleFile:
+    pickle.dump(energies, pickleFile)
+    
+with open("Saved matrices/"+folderName+"/pickledAtomicSymbolsList.txt", "wb") as pickleFile:
+    pickle.dump(atomicSymbolsList, pickleFile)
 
 print("DONE")
 
