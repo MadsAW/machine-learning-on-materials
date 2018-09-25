f2120a5b06e8bc12a393c39d6be8881fa0a520a8
Simon Kamuk <simon.kamuk@gmail.com>
2018-09-25 12:34:58 +0200

    Sorting script now uses loaded data instead of database
    Sorting script now uses loaded data instead of database. Also found bug in old sorting: atomic numbers were not unique, so were counted too many times. Cutoff now lowered to eg 100, because 350 cuts everything off.

--- sortFeatureMatrix.py
+++ sortFeatureMatrix.py
@@ -5,10 +5,12 @@ Created on Wed Sep 19 14:25:16 2018
 
 @author: Simon
 """
-from ase.db import connect
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+import pandas as pd
+import os
 
 
 
@@ -16,15 +18,21 @@ import pickle
 #Fjerner features hvor der er færre end (cutOff) materialer som indeholder grundstofferne
 
 
-data = connect('oqmd12.db')
+atomicSymbolsAndNumbers=np.array(pd.read_csv("atomicSymbolsAndNumbers.csv", header=None, sep=","))
+def getAtomicNumbers(atomicSymbols):
+    out=[]
+    for elem in atomicSymbols:
+        for i in atomicSymbolsAndNumbers:
+            if elem == i[1]:
+                out.append(i[0])
+    
+    return(out)
+    print("Atomic symbol not found in list")
+    assert(False)
 
-atomsList=[]
-
-for row in data.select():
-    atomsList.append(data.get_atoms(row['id']))
 
 #Path to saved files
-path = "Saved matrices/24-09-2018 21.37 TEST/"
+path = "Saved matrices/24-09-2018 21.44/"
 
 featureMatrix = np.load(path+"/featureMatrix.npy")
 
@@ -32,18 +40,17 @@ with open(path+"/pickledEnergies.txt", "rb") as pickleFile:
     energies = pickle.load(pickleFile)
     
 with open(path+"/pickledAtomicSymbolsList.txt", "rb") as pickleFile:
-    atomicSymbolsList = pickle(pickleFile)
+    atomicSymbolsList = pickle.load(pickleFile)
 
-#Koden skal laves om så ovenstående 3 loadede matricer bruges i stedet for at starte det hele forfra.
 
-atomsRemovedNotTwo=[]
-atomsRemovedCutOff=[]
+crystalsRemovedNotTwo=[]
+crystalsRemovedCutOff=[]
 indicesToRemove=[]
 
 
 atomicNumberCounts={}
-for atom in atomsList:
-    for element in atom.get_atomic_numbers():
+for crystal in atomicSymbolsList:
+    for element in getAtomicNumbers(crystal):
         if element not in atomicNumberCounts:
             atomicNumberCounts[element]=1
         else:
@@ -51,28 +58,25 @@ for atom in atomsList:
 
 #Atomic number occurences as an np array, sorted by number atomic number.
 plotData=np.array(sorted(atomicNumberCounts.items()))
-cutOff = 350
+cutOff = 100
 newPlotData = plotData[plotData[:,1]>cutOff]
 removedPlotData = plotData[plotData[:,1]<=cutOff]
 
 
 
 index=0
-for atom in atomsList:
+for crystal in atomicSymbolsList:
     if len(atomicSymbolsList[index])!=2:
-        atomsRemovedNotTwo.append(atom)
+        crystalsRemovedNotTwo.append(crystal)
         indicesToRemove.append(index)
-    elif any(elem in removedPlotData[:,0] for elem in atom.get_atomic_numbers()):
-        atomsRemovedCutOff.append(atom)
+    elif any(elem in removedPlotData[:,0] for elem in getAtomicNumbers(crystal)):
+        crystalsRemovedCutOff.append(crystal)
         indicesToRemove.append(index)
     index+=1
 
 newFeatureMatrix=np.delete(featureMatrix,indicesToRemove,0)
 newAtomicSymbolsList = list(np.delete(atomicSymbolsList,indicesToRemove,0))
-
-chemFormulaeNotTwo=[]
-for i in range(len(atomsRemovedNotTwo)):
-    chemFormulaeNotTwo.append(atomsRemovedNotTwo[i].get_chemical_formula())
+newEnergies = list(np.delete(energies,indicesToRemove,0))
 
 print(f"Removed {len(featureMatrix)-len(newFeatureMatrix)} entries from the feature matrix")
 
@@ -83,11 +87,21 @@ plt.bar(removedPlotData[:,0],removedPlotData[:,1], color='red')
 plt.plot([-1,max(plotData[:,0])+2],[cutOff,cutOff], color='red')
 plt.show()
 
-removedAtomicNumbers=removedPlotData[:,0]
 
 
 
+try:
+    os.mkdir(path+f"sorted_Cutoff{cutOff}_noSingleElementKrystals/")
+except:
+    pass
+newFolder=path+f"sorted_Cutoff{cutOff}_noSingleElementKrystals/"
 
+np.save(newFolder+"featureMatrix.npy", newFeatureMatrix)
 
-np.save(f"featureMatrixCutoff{cutOff}_noSingleElementKrystals.npy", newFeatureMatrix)
-np.save(f"atomicSymbolsListCutoff{cutOff}_noSingleElementKrystals.npy", newAtomicSymbolsList)
+with open(newFolder+"/pickledEnergies.txt", "wb") as pickleFile:
+    pickle.dump(newEnergies, pickleFile)
+    
+with open(newFolder+"/pickledAtomicSymbolsList.txt", "wb") as pickleFile:
+    pickle.dump(newAtomicSymbolsList, pickleFile)
+    
+print("DONE")
