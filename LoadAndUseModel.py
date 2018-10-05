3aa0738b20116d55ea494918ab0baf0cb29a6333
Nightcrashdash <40065771+Nightcrashdash@users.noreply.github.com>
2018-10-05 14:18:50 +0200

    Added a file which loads and uses prevoius models

---
+++ LoadAndUseModel.py
@@ -0,0 +1,34 @@
+from keras.models import load_model
+from keras import backend
+import pickle
+from createLargerFeatureMatrix import simpleLargeMatrix
+import numpy as np
+
+
+path = "Saved matrices/26-09-2018 15.23/sorted_Cutoff25_noSingleElementKrystals/"
+featureMatrixFile = "featureMatrix.npy"
+atomicSymbolsListFile = "pickledAtomicSymbolsList.txt"
+
+
+largeFeatureMatrix, mappedAtomicNumber = simpleLargeMatrix(path,featureMatrixFile, atomicSymbolsListFile)
+with open(path+"pickledEnergies.txt", "rb") as pickleFile:
+    energies = pickle.load(pickleFile)
+
+X = largeFeatureMatrix
+Y = np.array(energies)
+
+#model = Sequential()
+def rmse(y_true, y_pred):
+	return backend.sqrt(backend.mean(backend.square(y_pred - y_true), axis=-1))
+
+model_path=path+'test2_model.h5'
+model = load_model(model_path, custom_objects={'rmse': rmse})
+
+predictions = model.predict(X)
+
+#Save weights
+a=0
+for i in range(len(predictions)):
+    a+=(energies[i]-predictions[i])**2
+rmse=np.sqrt(a/len(energies))
+print(rmse)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # machine-learning-on-materials
 This is project is meant for an implementation of machine learning into a material database. The goal is to be able to predict the properties of the materials.
+
+---
