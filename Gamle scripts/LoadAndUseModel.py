1a63eccb43013dceccf41b6a11194a8f8a9e1ec6
Simon Kamuk <simon.kamuk@gmail.com>
2018-10-12 16:15:37 +0200

    Deleted old NN training console output

--- Gamle scripts/LoadAndUseModel.py
+++ Gamle scripts/LoadAndUseModel.py
@@ -6,12 +6,13 @@ import numpy as np
 from keras.utils import CustomObjectScope
 
 
-
+#Define path to model that we want to load
 path = "Saved matrices/26-09-2018 15.23/sorted_Cutoff25_noSingleElementKrystals/"
+#Define the matrix files
 featureMatrixFile = "featureMatrix.npy"
 atomicSymbolsListFile = "pickledAtomicSymbolsList.txt"
 
-
+#Create feature matrix in RAM
 largeFeatureMatrix, mappedAtomicNumber = simpleLargeMatrix(path,featureMatrixFile, atomicSymbolsListFile)
 with open(path+"pickledEnergies.txt", "rb") as pickleFile:
     energies = pickle.load(pickleFile)
@@ -19,19 +20,24 @@ with open(path+"pickledEnergies.txt", "rb") as pickleFile:
 X = largeFeatureMatrix
 Y = np.array(energies)
 
-#model = Sequential()
+#Define custom method used in model
 def rmse(y_true, y_pred):
 	return backend.sqrt(backend.mean(backend.square(y_pred - y_true), axis=-1))
 
+<<<<<<< HEAD:LoadAndUseModel.py
+#Load model
+=======
 
+>>>>>>> 28ba819a50312b5b4bd37d4ccb110cad9c30a1b6:Gamle scripts/LoadAndUseModel.py
 model_path=path+'test2_model.h5'
 
 with CustomObjectScope({'rmse': rmse}):
     model = load_model(model_path)
 
-predictions = model.predict(X)
+#predict
+predictions = model.predict(X[0:10])
 
-#Save weights
+#Judge how good the prediction is
 a=0
 for i in range(len(predictions)):
     a+=(energies[i]-predictions[i])**2
--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 This is project is meant for an implementation of machine learning into a material database. The goal is to be able to predict the properties of the materials.
 
 ---
+## General flow of the projects
+
+The project is structured around several files and matrices that are used before the actual Neural Network (NN) is implemented. As such the project is effectively divided up into the following bits:
+
+### Preprocessing of data
+
+Here is an overview of the general flow when preprocessing data
+
+|File|Description| Output|
+|----|------------------------------------|-------|
+|createFeatureMatrix|This file creates the feature matrix. This is done using prdf using the variables rs dr and rmax. The featrue matrix, energy list and atomicsymbol list are then Pickled|featureMatrix.npy, pickledEnergies.txt and pickledAtomicSymbolsList.txt|
