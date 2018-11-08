f9d64b34d575d0362a3fa83c8fbab54b2d5d5e89
Simon Kamuk <simon.kamuk@gmail.com>
2018-11-08 15:47:40 +0100

    fixed test10

--- kernel1.py
+++ kernel1.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 26 11:57:35 2018
+Created on Thu Nov  8 13:54:00 2018
 
 @author: Simon
 """
+
+
 import os
 from createLargerFeatureMatrix import simpleLargeMatrix
 import pickle
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras import regularizers
 import numpy as np
 
 path = "Saved matrices/11-10-2018 11.36/sorted_Cutoff25_noSingleElementKrystals/"
@@ -31,50 +30,40 @@ Y = np.array(energies)
 
 
 
-#Lambda regularization penalty
-drop = 0.2
 
 
-#Create model
-model = Sequential()
-
-inputShape = np.shape(X)[1:]
-
-model.add(Dense(800, input_shape=inputShape, activation='relu'))
-model.add(Dropout(drop))
-model.add(Dense(400, activation='relu'))
-model.add(Dropout(drop))
-model.add(Dense(1))
+X=np.array([[1,2,3],[4,5,6],[7,8,9]])
+Y=np.array([10,11,12])
 
 
 
-#Compile model
-model.compile(loss='mse', optimizer='adam', metrics=["mse"])
 
-print(model.summary())
+lam = 3
+I = np.identity(len(X))
 
-#Fit the model. This is where the hard computing happens. 
-#Number of epochs is number of iterations through dataset
-#Batch size is number of iterations before weights are changed.
-model.fit(X, Y, epochs=40, batch_size=50)
+c=1
+def f(xi,xj):
+    return (xi @ xj)+c
 
-#Evaluate model efficiency
-scores = model.evaluate(X, Y)
-print("\n%s: %.2f eV" % (model.metrics_names[1], scores[1]))
-
-#Make predictions
-predictions = model.predict(X)
-
-#Save weights
-model.save_weights(os.path.basename(__file__)[:-3]+"weights")
+K = np.zeros(X.shape)
+for i in range(0,int(np.ceil(len(X)/2))+1):
+    for j in range(0,int(np.ceil(len(X)/2))+1):
+        K[i,j] = f(X[i],X[j])
+        K[j,i] = f(X[i],X[j])
 
 
-a=0
-for i in range(len(predictions)):
-    a+=(energies[i]-predictions[i])**2
-rmse=np.sqrt(a/len(energies))
 
-print("RMSE on training data "+str(rmse))
+diff=[]
+
+for i in range(len(X)):
+    x=X[i]
+    kap = np.zeros(x.shape)
+    for i in range(len(X)):
+        kap[i] = f(X[i],x)
+        
+    y=(Y.T @ np.linalg.inv(np.matrix(K+lam*I)) @ kap)
+    
+    diff.append((Y[i]-y)[0,0])
 
 
 
@@ -100,6 +89,10 @@ largeFeatureMatrixValidate.shape = (largeFeatureMatrixValidate.shape[0], -1)
 X_v = largeFeatureMatrixValidate
 Y_v = np.array(energiesValidate)
 
+
+
+
+"""
 #Make predictions on validation set
 
 predictionsValidate = model.predict(X_v)
@@ -117,3 +110,4 @@ print("RMSE on validation data "+str(rmseValidate))
 
 
 print("DONE")
+"""
