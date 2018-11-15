4641da978fca1aad0e9649db9a6d951f58fc27f3
Simon Kamuk <s173977@student.dtu.dk>
2018-11-15 13:43:45 +0100

    drop N act plot sh file

---
+++ drop_N_act_plot.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+##Kør på gpu
+#BSUB -q gputitanxpascal
+##Antal gpuer vi vil bruge. Kommenter ud hvis cpu.
+#BSUB -gpu "num=1:mode=exclusive_process"
+
+##Kør på cpu
+##BSUB -q hpc
+
+##Navn på job
+#BSUB -J drop_N_act_plot
+##Output fil
+#BSUB -o drop_N_act_plot-%J.out
+##Antal kerner
+#BSUB -n 5
+##Om kernerne må være på forskellige computere
+#BSUB -R "span[hosts=1]"
+##Ram pr kerne
+#BSUB -R "rusage[mem=6GB]"
+##Hvor lang tid må den køre hh:mm
+#BSUB -W 10:00
+##Email når jobbet starter
+#BSUB -B
+##og stopper
+#BSUB -N
+
+
+module purge
+#module load tensorflow/1.5-cpu-python-3.6.2
+module load tensorflow/1.5-gpu-python-3.6.2
+
+
+for drop in 0.30 0.50 0.80
+do
+	for N in 500 1000
+	do
+		for act in softmax elu selu softplus softsign relu tanh sigmoid hard_sigmoid exponential linear
+		do
+			python3 drop_N_act_plot.py $drop $N $act
+		done
+	done
+done
+
