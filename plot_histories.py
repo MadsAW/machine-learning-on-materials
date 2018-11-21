b91427e8ea6481332ec0e45be9c1689cca35c6f8
Simon Kamuk <simon.kamuk@gmail.com>
2018-11-21 16:40:06 +0100

    Update plot_histories.py

--- plot_histories.py
+++ plot_histories.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 os.chdir("histories")
 
-files = [file for file in os.listdir() if file!='plots']
+files = [file for file in os.listdir() if file!='plots' and file!='.DS_Store']
 for file in files:
     with open(file, "rb") as pickleFile:
         history = pickle.load(pickleFile)
@@ -21,11 +21,12 @@ for file in files:
     rmse_train = [elem**(1/2) for elem in history['mean_squared_error']]
     rmse_val = [elem**(1/2) for elem in history['val_mean_squared_error']]
     
-    plt.plot(rmse_train)
-    plt.plot(rmse_val)
-    plt.legend(('train','val'))
+    plt.plot(rmse_train, 'b', label='train')
+    plt.plot(rmse_val, 'r', label='validation')
+    plt.legend()
     plt.xlabel('epochs')
     plt.ylabel('Root mean squared error')
+    plt.ylim(0.1,0.6)
     plt.savefig('plots/'+file+'.png')
     plt.show()
     
