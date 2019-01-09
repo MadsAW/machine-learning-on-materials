de8bdd6d75359a653b3f3e58f7e5c7f9185b5515
Nightcrashdash <40065771+Nightcrashdash@users.noreply.github.com>
2019-01-09 12:55:03 +0100

    PLOTS

--- KRR/Saved/Run 4/plots.py
+++ KRR/Saved/Run 4/plots.py
@@ -2,15 +2,61 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 folder = "GP/"
-ktype = "laplace"
-matrix = "/4_"+ktype+"_val_0.01"
-with open(folder+ktype+matrix, "rb") as pickleFile:
-    results = pickle.load(pickleFile)
-    arrray = results[2]
-    # Enable interactive mode
-    plt.ion()
-    
-    # Draw the grid lines
-    plt.grid(True)
-    plt.plot(results[1],results[2])
-    plt.xscale('symlog', linthreshx=20)
+ktype = "lin"
+lambd = ["0.01","0.1","1.0"]
+plt.figure(0)   
+for l in lambd:
+    matrix = "/4_"+ktype+"_val_"+l
+    with open(folder+ktype+matrix, "rb") as pickleFile:
+        results = pickle.load(pickleFile)
+        array = results[2]
+        # Enable interactive mode
+        plt.ion()
+        plt.title("Linear kernel with GP description")
+        plt.xlabel("first coefficient (c) value")
+        plt.ylabel("rmse [eV]")
+        # Draw the grid lines
+        plt.grid(True)
+        plt.plot(results[1],results[2], marker='x', linestyle='dashed', linewidth=2, markersize=8, label="using lambda = "+l)
+        plt.xscale('symlog', linthreshx=20)
+        plt.ylim(0.1,0.5)
+        plt.legend(loc='upper left')
+        plt.show()
+plt.figure(1)   
+ktype="gauss"
+for l in lambd:
+    matrix = "/4_"+ktype+"_val_"+l
+    with open(folder+ktype+matrix, "rb") as pickleFile:
+        results = pickle.load(pickleFile)
+        array = results[2]
+        # Enable interactive mode
+        plt.ion()
+        plt.title("Gaussian kernel with GP description")
+        plt.xlabel("first coefficient (c) value")
+        plt.ylabel("rmse [eV]")
+        # Draw the grid lines
+        plt.grid(True)
+        plt.plot(results[1],results[2], marker='x', linestyle='dashed', linewidth=2, markersize=8, label="using lambda = "+l)
+        plt.ylim(0.2,0.7)
+        plt.legend(loc='upper left')
+        plt.show()
+        plt.savefig("gauss.png")
+ktype="laplace"
+plt.figure(2)   
+for l in lambd:
+    matrix = "/4_"+ktype+"_val_"+l
+    with open(folder+ktype+matrix, "rb") as pickleFile:
+        results = pickle.load(pickleFile)
+        array = results[2]
+        # Enable interactive mode
+        plt.ion()
+        plt.title("Laplacian kernel with GP description")
+        plt.xlabel("first coefficient (c) value")
+        plt.ylabel("rmse [eV]")
+        # Draw the grid lines
+        plt.grid(True)
+        plt.plot(results[1],results[2], marker='x', linestyle='dashed', linewidth=2, markersize=8, label="using lambda = "+l)
+        plt.ylim(0.2,0.7)
+        plt.legend(loc='upper left')
+        plt.show()
+        plt.savefig("lap.png")
