37add5c51beaaba864aabec9cf4a891ac4cd4b26
Nightcrashdash <40065771+Nightcrashdash@users.noreply.github.com>
2019-01-14 11:10:12 +0100

    last shell

---
+++ KRR/Shellscripts/batch/3/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##Kør på cpu
+#BSUB -q hpc
+##Navn på job
+#BSUB -J testing
+##Output fil
+#BSUB -o output/4_0.1_faulty_gauss-%J.out
+##Antal kerner
+#BSUB -n 1
+##Om kernerne må være på forskellige computere
+#BSUB -R "span[hosts=1]"
+##Ram pr kerne
+#BSUB -R "rusage[mem=10GB]"
+##Hvor lang tid må den køre hh:mm
+#BSUB -W 45:00
+##Email når jobbet starter
+#BSUB -B
+##og stopper
+#BSUB -N
+module purge
+module load python3
+python3 KRR/KRR_on_test.py linear 0.8193310105066505 GP newest -0.0004232564074429044
